75df94b8bdcf2c96caf967251ef083ba20aa750c
JJHaeun <yj05037@gmail.com>
2023-08-08 15:56:42 +0900

    make array, gugudan

---
+++ test2.php
@@ -0,0 +1,20 @@
+<?php
+
+// 변수를 배열로
+// 변수명  = array()
+
+// 변수를 배열로 선언한다
+$space = array();
+
+// 변수 space에 첫번째 인자로 earth를 넣어주기위해 인덱스 사용
+$space[0] = "earth";
+// 두번째로 star
+$space[1] = "star";
+// 세번째로 jupiter
+$space[2] = "jupiter";
+
+echo $space[0] . " , " . $space[1] . " , " . $space[2]
+
+
+//array('인덱스1'=>'값')
+?>
