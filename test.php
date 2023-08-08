8124b0e19648a361a84f0d3b7da67e308f66bffd
JJHaeun <yj05037@gmail.com>
2023-08-08 14:27:54 +0900

    상수,변수ì ã학습

--- test.php
+++ test.php
@@ -2,5 +2,20 @@
 echo '안녕하세요';
 echo '<br>';
 echo '반갑습니다';
+echo 1234;
 echo '<br>';
-?>깃
+//php주우ㅠ성!!
+print 'php세계 <br>';
+// 주석
+$strHi = '안녕';
+$strSay = "반가워요";
+
+echo $strHi . '하세요' . $strSay;
+define("FAVORITE_DOLL", "gelatoni");
+
+echo '<br> 상수 FAVORITE_DOLL은' . FAVORITE_DOLL;
+define("FAVORITE_DOLL", "gablielle");
+echo '<br> 상수 FAVORITE_DOLL은' . FAVORITE_DOLL;
+$sum = 5 + 5;
+echo $sum
+?>
